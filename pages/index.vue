628e3ed766473abb54c38b1e0d47eb18ebaf4618
gumrel <gumrel@MacBook-Air-gumrel.local>
2024-11-01 02:32:41 -0700

    start project

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import Comments from "~/components/Comments.vue";
-
 useHead({
   title: "ikakprosto.ru",
 });
@@ -10,14 +8,7 @@ useHead({
 
 <template>
   <div>
-    <link
-      href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
-      rel="stylesheet"
-    />
     <NuxtRouteAnnouncer />
-    <h1 style="color: #ff6b00" class="text-4xl font-bold text-center mt-5">
-      ikakprosto.ru
-    </h1>
-    <Posts />
+    <p>start</p>
   </div>
 </template>
